35208e5446549d2fac9be6c0ef367d28c3b46adf
HariomZecdata <hariom.a@zecdata.com>
2026-06-17 13:49:52 +0530

    Fix Jest and Babel config - all tests passing

---
+++ jest.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.js'],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+  },
+  collectCoverageFrom: ['*.js', '!node_modules/**'],
+};
--- jest.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    testEnvironment: 'node',
-    testMatch: ['**/tests/**/*.test.js'],
-    transform: {
-        '^.+\\.js$': 'babel-jest',
-    },
-    collectCoverageFrom: ['*.js', '!node_modules/**'],
-};
